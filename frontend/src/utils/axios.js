daf9385db07bdb611d011f3579cb089cc1afe32f
Niall1001 <niallwalters135@gmail.com>
2022-02-26 20:18:35 +0000

    log

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 const instance = axios.create({
 	baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/'
 });
+console.log(instance.baseURL)
 
 export default instance;
